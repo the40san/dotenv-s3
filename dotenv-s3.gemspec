f4c01b8e901a1e7cb399531ef74c4a2ba1a4e0e4
Masashi AKISUE <masashi.akisue@aktsk.jp>
2016-08-30 13:26:41 +0900

    initial implementation

--- dotenv-s3.gemspec
+++ dotenv-s3.gemspec
@@ -23,11 +23,13 @@ Gem::Specification.new do |spec|
   end
 
   spec.files         = `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
-  spec.bindir        = "exe"
-  spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
+  spec.bindir        = "bin"
+  spec.executables   = ["dotenv-s3"]
+
   spec.require_paths = ["lib"]
 
   spec.add_dependency 'aws-sdk', '~> 2'
+  spec.add_dependency 'thor'
   spec.add_development_dependency "bundler", "~> 1.11"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
--- lib/dotenv/s3.rb
+++ lib/dotenv/s3.rb
@@ -1,7 +1,52 @@
 require "dotenv/s3/version"
+require "base64"
+require "aws-sdk"
 
 module Dotenv
   module S3
-    # Your code goes here...
+    class << self
+      def load(bucket: nil, filename: nil, base64_encoded: false, kms_key_id: nil)
+        dotenv_body = s3_client.get_object(bucket: bucket, key: filename).body.read
+
+        if kms_key_id
+          blob = decode64(dotenv_body)
+          dotenv_body = decrypt_by_kms(blob)
+        end
+
+        dotenv_body = decode64(dotenv_body) if base64_encoded
+
+        create_dotenv(dotenv_body)
+      end
+
+      def s3_client
+        Aws::S3::Client.new(
+          access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+          secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+          region:            ENV['AWS_REGION']
+        )
+      end
+
+      def kms_client
+        Aws::KMS::Client.new(
+          access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+          secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+          region:            ENV['AWS_REGION']
+        )
+      end
+
+      def decode64(body)
+        Base64.decode64(body)
+      end
+
+      private
+
+      def decrypt_by_kms(body)
+        kms_client.decrypt(ciphertext_blob: body).plaintext
+      end
+
+      def create_dotenv(body)
+        File.open('.env', 'w').write(body)
+      end
+    end
   end
 end
