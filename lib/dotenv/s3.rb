5b07e35ec9a1226b13230e01c0f464bac9c29d05
Masashi AKISUE <masashi.akisue@aktsk.jp>
2016-08-30 15:36:22 +0900

    rails support

--- lib/dotenv/s3.rb
+++ lib/dotenv/s3.rb
@@ -1,6 +1,8 @@
-require "dotenv/s3/version"
-require "base64"
-require "aws-sdk"
+require 'dotenv/s3/version'
+require 'dotenv/s3/rails'
+require 'base64'
+require 'aws-sdk'
+require 'tempfile'
 
 module Dotenv
   module S3
@@ -15,7 +17,13 @@ module Dotenv
 
         dotenv_body = decode64(dotenv_body) if base64_encoded
 
-        create_dotenv(dotenv_body)
+        if block_given?
+          create_tempfile(dotenv_body) do |f|
+            yield f
+          end
+        else
+          create_dotenv(dotenv_body)
+        end
       end
 
       def s3_client
@@ -42,6 +50,14 @@ module Dotenv
 
       def decrypt_by_kms(body)
         kms_client.decrypt(ciphertext_blob: body).plaintext
+      end
+
+      def create_tempfile(body, &block)
+        Tempfile.create("s3-dotenv") do |f|
+          f.write(body)
+          f.rewind
+          yield f
+        end
       end
 
       def create_dotenv(body)
