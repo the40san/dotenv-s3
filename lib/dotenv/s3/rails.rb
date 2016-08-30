dd722ef8f53cce2d2a886486ae4b85463ad219c9
Masashi AKISUE <masashi.akisue@aktsk.jp>
2016-08-30 17:19:54 +0900

    fix undefined constant rails

--- lib/dotenv/s3/rails.rb
+++ lib/dotenv/s3/rails.rb
@@ -1,17 +1,19 @@
-module Dotenv
-  module S3
-    class Railtie < Rails::Railtie
-      config.before_configuration { load_from_tempfile }
+if defined?(Rails)
+  module Dotenv
+    module S3
+      class Railtie < Rails::Railtie
+        config.before_configuration { load_from_tempfile }
 
-      def self.load_from_tempfile
-        if ENV["ENABLE_S3_ENVFILE"]
-          Dotenv::S3.load(
-            bucket:         ENV["ENVFILE_BUCKET_NAME"],
-            filename:       ENV["ENVFILE_FILE_NAME"],
-            base64_encoded: ENV["ENVFILE_BASE64ENCODE"].present?,
-            kms_key_id:     ENV["ENVFILE_KMS_KEY_ID"]
-          ) do |tempfile|
-            Dotenv.load(tempfile.path)
+        def self.load_from_tempfile
+          if ENV["ENABLE_S3_ENVFILE"]
+            Dotenv::S3.load(
+              bucket:         ENV["ENVFILE_BUCKET_NAME"],
+              filename:       ENV["ENVFILE_FILE_NAME"],
+              base64_encoded: ENV["ENVFILE_BASE64ENCODE"].present?,
+              kms_key_id:     ENV["ENVFILE_KMS_KEY_ID"]
+            ) do |tempfile|
+              Dotenv.load(tempfile.path)
+            end
           end
         end
       end
